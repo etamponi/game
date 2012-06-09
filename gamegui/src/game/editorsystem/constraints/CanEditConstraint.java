26400f730c974aa0ba390e5308644b776952bbcb
Emanuele Tamponi <emanuele.tamponi@diee.unica.it>
2012-06-09 18:09:31 +0200

    Add license header and license file

--- gamegui/src/game/editorsystem/constraints/CanEditConstraint.java
+++ gamegui/src/game/editorsystem/constraints/CanEditConstraint.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Emanuele Tamponi.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Emanuele Tamponi - initial API and implementation
+ ******************************************************************************/
 package game.editorsystem.constraints;
 
 import game.editorsystem.Editor;
