061102857f5afdb827b2312703b8bc92ef6371ba
Emanuele Tamponi <emanuele.tamponi@diee.unica.it>
2012-07-02 18:11:41 +0200

    Initial work on OneValueEncoder (needed for the Weka binding)

---
+++ gamelib/src/game/plugins/encoders/OneValueEncoder.java
@@ -0,0 +1,15 @@
+package game.plugins.encoders;
+
+public class OneValueEncoder extends LabelEncoder {
+	
+	public OneValueEncoder() {
+		setInternalOptions("labelMapping");
+	}
+
+	@Override
+	protected void updateSingleMapping() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
