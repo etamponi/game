f0aa43859e5f7e92c083a60a404e9d6a9c287890
Emanuele Tamponi <emanuele.tamponi@diee.unica.it>
2013-02-26 00:41:03 +0100

    A very very basic version of the prepare() gui interface

--- gameguilib/src/game/plugins/editors/DatasetBuilderEditor.java
+++ gameguilib/src/game/plugins/editors/DatasetBuilderEditor.java
@@ -1,5 +1,39 @@
 package game.plugins.editors;
 
+import game.core.DatasetBuilder;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+
 public class DatasetBuilderEditor extends IObjectEditor {
+	
+	private Button prepareButton;
+	
+	public DatasetBuilderEditor() {
+		prepareButton = new Button("Prepare");
+		prepareButton.setPrefWidth(75);
+		prepareButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				if (getModel() != null) {
+					DatasetBuilder builder = getModel().getContent();
+					if (builder != null)
+						builder.prepare();
+				}
+			}
+		});
+	}
+
+	@Override
+	public void updateView() {
+		super.updateView();
+		
+		getPane().add(prepareButton, 1, getSubEditorCount()+2);
+	}
+
+	@Override
+	public Class getBaseEditableClass() {
+		return DatasetBuilder.class;
+	}
 	
 }
